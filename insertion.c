12fde2d5807419474642236f02d4b389c3131182
sovon1 <144060315+sovon1@users.noreply.github.com>
2024-03-22 12:51:48 +0600

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -5,6 +5,7 @@ int main()
  int i,n, array[50];
  printf("Enter size : ");
  scanf("%d",&n);
+ printf("Plese enter array elements  : \n ");
  for (i=0;i<n;i++)
  {
   scanf("%d",&array[i]);
@@ -15,6 +16,16 @@ int main()
  int pos,num;
  printf("at which position do you want to insert?: ");
 scanf("%d",&pos);
+//manualy checking bound
+if (pos>n || pos<0)
+//there might be a bug for if (pos >= n || pos < 0)
+//i will deal with it later 
+
+{
+    printf("This area is out of bound , please enter valid range ");
+}
+else 
+{
 printf("Enter num:");
 scanf("%d",&num);
 for(i=n-1;i>=pos-1;i--)
@@ -23,9 +34,16 @@ array[i+1] = array[i];
 }
 array[pos-1]=num;
 n++;
+ // also you could use 
+ // for (int i = n; i > pos; i--) {
+ //        array[i] = array[i - 1];
+ //    }
+    // array[pos] = num;
+    // n++;
 
 for(i=0;i<n;i++)
 {
  printf("%d ",array[i]);
+}
 }
 }
